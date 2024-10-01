ae65bf0ca7267c137eef7ee2ef34f12b637fd869
MikeySharma <mikeysharma99@gmail.com>
2024-10-01 01:02:30 -0700

    basics 1 completed

---
+++ py-basics/datatypes-04.py
@@ -0,0 +1,42 @@
+x = "Python"
+print('type of x:', type(x))
+
+y = 20
+print("type of y:", type(y))
+
+z = 20.5
+print("type of z:", type(z))
+
+a = 20j
+print("type of a:", type(a))
+
+mylist = ['apple', 'banana', 'cherry']
+print('type of mylist:', type(mylist))
+
+my_tuple = ('apple', 'banana', 'cherry')
+print('type of my_tuple', type(my_tuple))
+
+my_range = range(6)
+print('type of my_range', type(my_range))
+
+my_dict = {'apple', 'banana', 'cherry'}
+print('type of my_dict: ', type(my_dict))
+
+my_frozenset = frozenset({'apple', 'banana', 'cherry'})
+print('type of my_frozenset: ', type(my_frozenset))
+
+my_bool = True
+print('type of my_bool: ', type(my_bool))
+
+my_bytes = b"Hello"
+print('type of my_bytes: ',type(my_bytes) )
+
+my_bytearray = bytearray(5)
+print('type of my_bytearray: ', type(my_bytearray))
+
+my_memory_view = memoryview(bytes(5))
+print('type of my_memory_view: ', type(my_memory_view))
+
+myNone = None
+print('type of myNone: ', type(myNone))
+
